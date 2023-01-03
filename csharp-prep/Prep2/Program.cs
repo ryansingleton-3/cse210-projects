a43c66944f4c2300ce6aa1da89949297c3f9d493
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-01-03 15:41:27 -0600

    completed prep sections

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string letter;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80 && percentage < 90)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70 && percentage < 80)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 600 && percentage < 70)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine(letter);
+
+        if (percentage < 70)
+        {
+            Console.WriteLine("You did not pass the class. Maybe you will on the next attempt!");
+        }
+        else 
+        {
+            Console.WriteLine("You passed the class! Congratulations!");
+        }
+
+
     }
 }
