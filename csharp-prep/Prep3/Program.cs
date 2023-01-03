6863fecbc38cc7e21666d9301a4d3817a0d83d4e
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-01-03 16:28:36 -0600

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is the magic number? ");
+        // string magicNumStr = Console.ReadLine();
+        // int magicNumber = int.Parse(magicNumStr);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        int guess = -1;
+
+        while ((guess != magicNumber))
+        {
+            Console.WriteLine("What your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+            Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+            Console.WriteLine("Lower");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            }
+        }
+        
+
     }
 }
