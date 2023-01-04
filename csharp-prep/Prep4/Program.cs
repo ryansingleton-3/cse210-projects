b76f8dc950d9dae6d854a50308019d043e8fef7d
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-01-03 19:19:09 -0600

    finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            string strInput = Console.ReadLine();
+            userNumber = int.Parse(strInput);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+
     }
 }
