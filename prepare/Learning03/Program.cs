7349ac448ec5a369db380db980be46ba3ed32091
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-02-01 14:58:20 -0500

    finished Learning3 activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction firstFraction = new Fraction();
+        Fraction secondFraction = new Fraction(6);
+        Fraction thirdFraction = new Fraction(6,7);
+        Console.WriteLine(firstFraction.GetFractionString());
+        Console.WriteLine(firstFraction.GetDecimalValue());
+        Console.WriteLine(secondFraction.GetFractionString());
+        Console.WriteLine(secondFraction.GetDecimalValue());
+        Console.WriteLine(thirdFraction.GetFractionString());
+        Console.WriteLine(thirdFraction.GetDecimalValue());
+
+
     }
 }
